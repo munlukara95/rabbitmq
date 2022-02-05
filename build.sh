e1eb7613e8df7ed7414d42bd6ee422f1901f10bb
munlukara <74314150+munlukara95@users.noreply.github.com>
2022-02-05 14:50:21 +0300

    feat(rabbitmq): updated rabbitmq docker image

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-docker image build -t olgac/rabbitmq:3.7.8-management .
-docker push olgac/rabbitmq:3.7.8-management
+docker image build -t via/rabbitmq:3.9.13-management .
+docker push via/rabbitmq:3.9.13-management
